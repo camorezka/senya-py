eb43343e879c70fe845f7a5db1522c03f24a08d0
camorezka <sadovoyura777333arbuz@gmail.com>
2025-12-25 00:51:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ app.add_middleware(
 app.add_middleware(
     SessionMiddleware,
     secret_key=SECRET_KEY,
-    same_site="none", 
+    same_site="lax", 
     https_only=False, 
 )
 
@@ -82,8 +82,7 @@ oauth.register(
     client_id=GOOGLE_CLIENT_ID,
     client_secret=GOOGLE_CLIENT_SECRET,
     server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
-    client_kwargs={"scope": "openid email profile"},
-    skip_authorization=True
+    client_kwargs={"scope": "openid email profile"
 )
 
 
